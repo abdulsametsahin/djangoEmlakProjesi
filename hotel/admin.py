dcb531f58ceb92dfc7c0e5bef728756e566ed331
Abdulsamet <abdulsametsah@gmail.com>
2021-05-23 22:58:58 +0300

    category admin

--- hotel/admin.py
+++ hotel/admin.py
@@ -16,3 +16,38 @@ class HotelAdmin(admin.ModelAdmin):
 class ImagesAdmin(admin.ModelAdmin):
     list_display = ['title', 'hotel', 'image_tag']
     readonly_fields = ('image_tag',)
+class CategoryAdmin2(DraggableMPTTAdmin):
+    mptt_indent_field = "title"
+    list_display = ('tree_actions', 'indented_title',
+                    'related_hotels_count', 'related_hotels_cumulative_count')
+    list_display_links = ('indented_title',)
+    prepopulated_fields = {'slug': ('title',)}
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+
+        # Add cumulative product count
+        qs = Category.objects.add_related_count(
+            qs,
+            Hotel,
+            'category',
+            'hotels_cumulative_count',
+            cumulative=True)
+
+        # Add non cumulative product count
+        qs = Category.objects.add_related_count(qs,
+                                                Hotel,
+                                                'category',
+                                                'hotels_count',
+                                                cumulative=False)
+        return qs
+
+    def related_hotels_count(self, instance):
+        return instance.hotels_count
+
+    related_hotels_count.short_description = 'Related hotels (for this specific category)'
+
+    def related_hotels_cumulative_count(self, instance):
+        return instance.hotels_cumulative_count
+
+    related_hotels_cumulative_count.short_description = 'Related hotels (in tree)'
